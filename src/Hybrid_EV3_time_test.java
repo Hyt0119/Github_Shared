009d8fda0b4e23e693e8c2a28d39e9013bd37c03
bp17078 <sishou.tou@gmail.com>
2019-12-17 20:25:56 +0900

    First commit.

---
+++ src/Hybrid_EV3_time_test.java
@@ -0,0 +1,104 @@
+/*
+ * PCとBluetooth通信をするEV3側のプログラム
+ * */
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import lejos.hardware.lcd.LCD;
+import lejos.remote.nxt.BTConnector;
+import lejos.remote.nxt.BTConnection;
+import lejos.utility.Delay;
+
+public class Hybrid_EV3_time_test {
+
+    public static void main(String[] args) {
+
+        /* 通信用インスタンスの変数の準備 */
+        BTConnection connection = null;
+        DataInputStream dis = null;
+        DataOutputStream dos = null;
+
+        /* コネクション用インスタンスの作成 */
+        BTConnector connector = new BTConnector();
+
+        LCD.clear();
+        LCD.drawString("wait connection", 0, 1);
+        /* コネクション確立 */
+        connection = connector.waitForConnection(0, BTConnection.RAW);
+
+        /* コネクション確立時のエラー処理 */
+        if (connection == null) {
+            LCD.clear();
+            LCD.drawString("connection error!", 0, 1);
+            Delay.msDelay(5000);
+            System.exit(1);
+        }
+
+        /* データ送受信用ストリームの作成 */
+        dis = connection.openDataInputStream();
+        dos = connection.openDataOutputStream();
+
+        int loop = 100;
+//        int buffer = -1;
+        String buffer = "";
+        for (int i = 0; i < loop; i++) {
+            try {
+                /* 整数の受信 */
+                buffer = dis.readUTF();
+
+            } catch (IOException ioe) {
+                /* 受信失敗時のエラー処理 */
+                LCD.clear();
+                LCD.drawString("reception error!", 0, 1);
+                Delay.msDelay(5000);
+                System.exit(1);
+            }
+
+            LCD.clear();
+            LCD.drawString("receive : " + String.valueOf(buffer), 0, 2);
+        }
+
+        for (int i = 0; i < loop; i++) {
+            try {
+//                buffer = "性欲FUCK100000000000000000FUCK/FUCK100000000000000000FUCK/FUCK100000000000000000FUCK/FUCK100000000000000000FUCK/FUCK100000000000000000FUCK/FUCK100000000000000000FUCK";
+buffer = "〒135-8548 東京都江東区豊洲３丁目７-５";
+                /* 整数の送信 */
+//                dos.writeInt(buffer);
+                dos.writeUTF(buffer);
+                dos.flush();
+
+            } catch (IOException ioe) {
+                /* 送信失敗時のエラー処理 */
+                LCD.clear();
+                LCD.drawString("sending error!", 0, 1);
+                Delay.msDelay(5000);
+                System.exit(1);
+            }
+
+            LCD.clear();
+            LCD.drawString("send : " + String.valueOf(buffer), 0, 2);
+            Delay.msDelay(100);
+        }
+
+        try {
+
+            /* データ送受信用ストリームのクローズ */
+            dos.close();
+            dis.close();
+
+            /* コネクション切断 */
+            connection.close();
+            connector.close();
+
+        } catch (IOException ioe) {
+            /* コネクション切断時のエラー処理 */
+            LCD.clear();
+            LCD.drawString("closing error!", 0, 1);
+            Delay.msDelay(5000);
+            System.exit(1);
+        }
+    }
+
+}
---
+++ src/Hybrid_PC_time_test.java
@@ -0,0 +1,116 @@
+/*
+ * EV3とBluetooth通信をするPC側のプログラム
+ * */
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+import javax.microedition.io.Connection;
+import javax.microedition.io.Connector;
+import javax.microedition.io.InputConnection;
+import javax.microedition.io.OutputConnection;
+
+public class Hybrid_PC_time_test {
+	public static int get_time(){
+		Date now = new Date();      
+		Long longTime = new Long(now.getTime() / 1000);
+		return longTime.intValue();
+	}
+
+    public static void main(String[] args) {
+
+        /*
+         * addressはEV3のMacアドレス
+         * ただし、bluecoveのアドレス指定では
+         * Macアドレスの前に"btspp://"と後に":1"を書き足す
+         * (例)Macアドレス : 00:16:53:4B:88:B9の場合
+         * 指定するアドレス = "btspp://0016534B88B9:1"
+         * */
+        final String address = "btspp://0016535DE375:1";
+
+        /* 通信用インスタンス用の変数の準備 */
+        Connection con = null;
+        DataOutputStream dos = null;
+        DataInputStream dis = null;
+
+
+        try {
+            /* 指定したアドレスとのコネクション確立 */
+            con = Connector.open(address);
+
+            /* データ送受信用のストリーム作成 */
+            dos = new DataOutputStream(((OutputConnection) con).openOutputStream());
+            dis = new DataInputStream(((InputConnection) con).openInputStream());
+
+        } catch (IOException ioe) {
+            /* コネクション確立失敗時のエラー処理 */
+            con = null;
+            dos = null;
+            dis = null;
+            System.exit(1);
+        }
+
+//        int loop = 100;
+        int now = get_time();
+        System.out.println("Start time:" + now);
+        while (get_time() - now < 20){
+//        for (int i = 0; i < loop; i++) {
+            try {
+                /* 整数の送信 */
+//                dos.writeInt(i);
+                dos.writeUTF(String.valueOf(get_time()));
+                dos.flush();
+
+            } catch (IOException ioe) {
+                /* 送信失敗時のエラー処理 */
+                System.err.println(ioe.getMessage());
+                System.exit(1);
+            }
+
+            System.out.println("send : " + String.valueOf(get_time()));
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ie) {
+                System.err.println(ie.getMessage());
+                System.exit(1);
+            }
+        }
+        
+        int receive_start = get_time();
+        String buffer = "";
+        while (get_time() - receive_start > 10){
+//        for (int i = 0; i < loop; i++) {
+            try {
+                /* 整数の受信 */
+//                buffer = dis.readInt();
+                buffer = dis.readUTF();
+
+            } catch (IOException ioe) {
+                /* 受信失敗時のエラー処理 */
+                System.err.println(ioe.getMessage());
+                System.exit(1);
+            }
+
+            System.out.println("receive : " + String.valueOf(buffer));
+            buffer = "";
+        }
+
+        try {
+            /* コネクション切断 */
+            con.close();
+
+            /* データ送受信用ストリームのクローズ */
+            dos.close();
+            dis.close();
+
+        } catch (IOException ioe) {
+            /* コネクション切断時のエラー処理 */
+            System.err.println(ioe.getMessage());
+            System.exit(1);
+        }
+
+    }
+
+}
